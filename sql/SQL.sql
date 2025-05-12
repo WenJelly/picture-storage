65a9b9063997c569b76a91921646edd954d7770f
wenguodong <wenguodong@atplatform.net>
2025-05-12 17:32:47 +0800

    部分代码优化

--- sql/SQL.sql
+++ sql/SQL.sql
@@ -2,16 +2,6 @@ create database if not exists smart_picture_storage_wenjelly default character s
 
 use smart_picture_storage_wenjelly;
 -- 用户表
--- 扩展设计
--- 1）如果要实现会员功能，可以对表进行如下扩展：
--- 1.给 userRole 字段新增枚举值 vip，表示会员用户，可根据该值判断用户权限
--- 2.新增会员过期时间字段，可用于记录会员有效期
--- 3.新增会员兑换码字段，可用于记录会员的开通方式
--- 4.新增会员编号字段，可便于定位用户并提供额外服务，并增加会员归属感
---
--- 2）如果要实现用户邀请功能，可以对表进行如下扩展：
--- 1.新增 shareCode 分享码字段，用于记录每个用户的唯一邀请标识，可拼接到邀请网址后面，比如 https://mianshiya.com/?shareCode=xxx
--- 2.新增 inviteUser 字段，用于记录该用户被哪个用户邀请了，可通过这个字段查询某用户邀请的用户列表。
 create table if not exists user
 (
     id            bigint auto_increment comment 'id' primary key,
